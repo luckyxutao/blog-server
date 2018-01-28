1d7f43ba27ab9907eff8ac5a815f7ec0e09615f6
tao.xu <tao.xu@qunar.com>
2018-01-28 12:50:11 +0800

    add first ver

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var userDao = require('../dao/userDao');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+// 增加用户
+router.get('/addUser', function (req, res, next) {
+  userDao.add(req, res, next);
+});
+
+router.get('/queryAll', function (req, res, next) {
+  userDao.queryAll(req, res, next);
+});
+
+module.exports = router;
